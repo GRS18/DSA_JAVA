1720c21841fbb656065a2f57040c66c900f49c4b
Girish Ravindra Shinde <104589353+GRS18@users.noreply.github.com>
2024-10-14 22:45:03 +0530

    Add files via upload

---
+++ stackdsa/Wellness_Control.java
@@ -0,0 +1,45 @@
+package stackdsa;
+
+import java.util.Scanner;
+
+public class Wellness_Control {
+
+	static boolean check_welness(String word) {
+		
+		Stack_Char obj = new Stack_Char();// object created
+		
+		obj.create_Stack(word.length());
+		
+		for (int index = 0; index < word.length(); index++) 
+		{
+			if (word.charAt(index) == '{')
+				
+				obj.push('{');
+			
+			else if (word.charAt(index) == '}') 
+			{
+				if (obj.is_Empty() != true)
+				{
+					char g = obj.pop();
+					
+				} else
+					
+					return false;
+			}
+		}
+		return obj.is_Empty();
+	}
+
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter data:");
+		
+		String word = in.next();// read word from user
+		
+		System.out.println(word + "is balanced:" + check_welness(word));
+		
+	}
+
+}
