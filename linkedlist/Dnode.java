b47ac05f8a3cfdf9e21c6fe7e9edc5ec6d67627d
Girish Ravindra Shinde <104589353+GRS18@users.noreply.github.com>
2024-10-16 21:44:43 +0530

    Add files via upload

---
+++ linkedlist/Dnode.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+public class Dnode {
+
+	int data;
+	Dnode left, right;
+
+	public Dnode(int data) {
+
+		this.data = data;
+		left = right = null;
+	}
+
+	Dnode n = new Dnode(10);
+}
